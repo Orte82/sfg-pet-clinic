b27f09ccf515e0d31d33bfa4ca4befb10f01051d
Orte <kortes29@gmail.com>
2019-11-12 22:39:55 +0200

    implementing service interfaces

---
+++ pet-clinic-data/src/main/java/com/orte/sfgpetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package com.orte.sfgpetclinic.services;
+
+
+import com.orte.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
