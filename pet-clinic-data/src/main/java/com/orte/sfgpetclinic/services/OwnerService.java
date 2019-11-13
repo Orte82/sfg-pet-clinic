162bafb3c61e8004da69e34383b732343c604938
Orte <kortes29@gmail.com>
2019-11-13 17:26:35 +0200

    Implement Map Based classes

--- pet-clinic-data/src/main/java/com/orte/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/orte/sfgpetclinic/services/OwnerService.java
@@ -2,8 +2,6 @@ package com.orte.sfgpetclinic.services;
 
 import com.orte.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
